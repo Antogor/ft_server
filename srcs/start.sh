91f96a49a0e8f430d7608850552648db8a832d9e
Antogor <antoniogarzoniniesta@gmail.com>
2020-04-12 20:50:16 +0200

    without phpmyadmin

--- srcs/start.sh
+++ srcs/start.sh
@@ -4,9 +4,9 @@ sudo apt-get upgrade -y
 
 #INSTALACION
 sudo apt-get -y install nginx && \
-#sudo apt-get -y install mariadb-server && \
+sudo apt-get -y install mariadb-server && \
 sudo apt-get -y install mysql-server && \
-sudo apt-get -y install php7.2-cli php7.2-fpm php7.2-mysql php7.2-json php7.2-opcache php7.2-mbstring php7.2-xml php7.2-gd php7.2-curl && \
+sudo apt-get -y install php-mysql && \
 sudo wget https://es.wordpress.org/wordpress-latest-es_ES.tar.gz && \
 tar -xf wordpress-latest-es_ES.tar.gz && \
 rm wordpress-latest-es_ES.tar.gz
