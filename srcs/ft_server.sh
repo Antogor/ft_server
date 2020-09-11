bd488635ef5bfc8b9737e3d9fcbebcd497b8f019
Antogor <antoniogarzoniniesta@gmail.com>
2020-09-11 18:48:07 +0200

    optimized .sh :rocket:

--- srcs/ft_server.sh
+++ srcs/ft_server.sh
@@ -1,68 +1,69 @@
 #!/bin/bash
 
 ## Download the latest version from wordpress
-echo "Downloading latest version from wordpress"
+echo -e "\e[32m Downloading latest version from wordpress... \e[0m"
 
-if wget https://es.wordpress.org/wordpress-latest-es_ES.tar.gz; then
-	echo "Latest version from wrodpress loaded"
+if wget https://es.wordpress.org/wordpress-latest-es_ES.tar.gz > history.log 2>error.log; then
+	echo -e "\e[32m Latest version from wrodpress loaded\n \e[0m"
 else
-	echo "Error download wordpress"
+	echo -e "\e[91m Error download wordpress\n \e[0m"
 	exit 1
 fi
 
 ## Config wordpress before Docker
-echo "Coping wordpress.config"
+echo -e "\e[32m Coping wordpress.config... \e[0m"
 
-if tar -xzvf wordpress-latest-es_ES.tar.gz; then
-	rm wordpress-latest-es_ES.tar.gz
-	cp wp-config.php wordpress/
-	echo "Wordpress.config copied"
+if tar -xzvf wordpress-latest-es_ES.tar.gz>> history.log 2>>error.log; then
+	rm wordpress-latest-es_ES.tar.gz>> history.log 2>>error.log
+	cp wp-config.php wordpress/>> history.log 2>>error.log
+	echo -e "\e[32m Wordpress.config copied\n \e[0m"
 else
-	echo "Error to copy wrodpress.config"
+	echo -e "\e[91m Error to copy wrodpress.config\n \e[0m"
 	exit 1
 fi
 
 ## Download the latest version from phpMyAdmin
-echo "Downloading phpMyAdmin"
+echo -e "\e[35m Downloading phpMyAdmin... \e[0m"
 
-if wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz; then
-	echo "phpMyAdmin downloaded"
+if wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz>> history.log 2>>error.log; then
+	echo -e "\e[35m phpMyAdmin downloaded\n \e[0m"
 else
-	echo "Error download phpMyAdmin"
+	echo -e "\e[91m Error download phpMyAdmin\n \e[0m"
 	exit 1
 fi
 
 ## Configure phpMyAdmin
-if tar -xzvf phpMyAdmin-5.0.2-all-languages.tar.gz; then
-	rm phpMyAdmin-5.0.2-all-languages.tar.gz
-	mv phpMyAdmin-5.0.2-all-languages phpMyAdmin
-	rm phpMyAdmin/config.sample.inc.php
-	cp -f config.inc.php phpMyAdmin/
-	echo "Php configured"
+echo -e "\e[35m Configuring phpMyAdmin...\e[0m"
+if tar -xzvf phpMyAdmin-5.0.2-all-languages.tar.gz>> history.log 2>>error.log; then
+	rm phpMyAdmin-5.0.2-all-languages.tar.gz>> history.log 2>>error.log
+	mv phpMyAdmin-5.0.2-all-languages phpMyAdmin >> history.log 2>>error.log
+	rm phpMyAdmin/config.sample.inc.php>> history.log 2>>error.log
+	cp -f config.inc.php phpMyAdmin/>> history.log 2>>error.log
+	echo -e "\e[35m Php configured\n \e[0m"
 else
-	echo "Error to configure php"
+	echo -e "\e[91m Error to configure php\n \e[0m"
 	exit 1
 fi
 
 ## Starting Ft_server
-echo Building Docker image...
+echo -e "\e[34m Building Docker image...\e[0m"
 
 ## Conditional to build the image, if not possible the script stop
-if docker build ../ -t ft_server:latest; then
-	echo Docker image builded, name of container: ft_server
+if docker build ../ -t ft_server:latest>> history.log 2>>error.log; then
+	echo -e "\e[34m Docker image builded, name of container: ft_server\n \e[0m"
 else
-	echo "Docker image couldn't was buildt"
+	echo -e "\e[91m Docker image couldn't was buildt\n \e[0m"
 	exit 1
 fi
 
 ## Starting container
-echo Loading container ft_server...
+echo -e "\e[34m Loading container ft_server...\e[0m"
 
 # Conditional to run the container, if not possible the script stop
-if docker run -p 80:80 -p 443:443 -td ft_server:latest; then
-	echo Container loaded and runing
+if docker run --name ft_server --rm -p 80:80 -p 443:443 -td ft_server:latest>> history.log 2>>error.log; then
+	echo -e "\e[34m Container loaded and runing\n \e[0m"
 else
-	echo "Docker container couldn't was loaded"
+	echo -e "\e[91m Docker container couldn't was loaded\n \e[0m"
 	exit 1
 fi
 
